9f6bffce4a5d25a2dbe6819e9f3fd33dc2ee93c0
JeantPro <148301202+JeantPro@users.noreply.github.com>
2023-10-18 21:41:23 -0400

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,13 +1,13 @@
 def  suma ( a , b ):
     return a  +  b
 
-def resta(a, b):
-    return a - b
+def resta ( a , b ):
+    return a  -  b
 
-def multiplicacion(a, b):
-    return a * b
+def multiplicacion( a , b ):
+    return a  *  b
 
-def division(a, b):
+def division( a , b ):
     if b == 0:
         return "Error: No se puede dividir por cero"
     else:
